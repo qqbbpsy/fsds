51573049219b790d49893dec4200a03a5ee0cbc4
qqUnknowUserqq <45928848+qqbbpsy@users.noreply.github.com>
2023-04-12 01:02:23 +0300

    Add files via upload

---
+++ jsdh.js
@@ -0,0 +1,27 @@
+document.addEventListener('contextmenu', function(event) {
+  if (event.target.nodeName.toLowerCase() === 'img') {
+    event.preventDefault();
+  }
+});
+
+(function() {
+    var disableSelectionCSS = "body{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;}";
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+
+    style.type = 'text/css';
+    style.id = 'disableSelectionCSS';
+    style.appendChild(document.createTextNode(disableSelectionCSS));
+    head.appendChild(style);
+
+    var body = document.body || document.getElementsByTagName('body')[0];
+    body.onselectstart = function() {
+        return false;
+    };
+})();
+
+document.onkeydown = function(e) {
+  if (e.keyCode === 123 || (e.ctrlKey && e.keyCode === 85)) {
+    return false;
+  }
+};
